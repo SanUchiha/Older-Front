43de2d3b0ac5293ec45f43047e95ab453ed3e409
Jose Antonio Sanchez Molina <jose.a.sanchez.molina@gmail.com>
2024-11-16 23:46:44 +0100

    Ajuste reoirte y filtrar por numero de socio en pagos

--- src/app/components/lista-pagos/lista-pagos.component.ts
+++ src/app/components/lista-pagos/lista-pagos.component.ts
@@ -4,12 +4,12 @@ import { PagoService } from '../../services/pago.service';
 import { Socio, SocioData } from '../../interfaces/socio';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { ModalPagoComponent } from "../modal-pago/modal-pago.component";
+import { ModalPagoComponent } from '../modal-pago/modal-pago.component';
 import { FormsModule } from '@angular/forms';
 import { DocumentosService } from '../../services/documentos.service';
 import { DialogEditPagoComponent } from '../dialog-edit-pago/dialog-edit-pago.component';
 import { MatDialog } from '@angular/material/dialog';
-import { ModalEliminarPagoComponent } from "../modal-eliminar-pago/modal-eliminar-pago.component";
+import { ModalEliminarPagoComponent } from '../modal-eliminar-pago/modal-eliminar-pago.component';
 import { SocioService } from '../../services/socio.service';
 
 @Component({
@@ -22,11 +22,10 @@ import { SocioService } from '../../services/socio.service';
     MatIconModule,
     ModalPagoComponent,
     FormsModule,
-    ModalEliminarPagoComponent
-  ]
+    ModalEliminarPagoComponent,
+  ],
 })
 export class ListaPagosComponent implements OnInit {
-
   listPagos: Pago[] = [];
   listSocios: Socio[] = [];
   id: number | undefined;
@@ -35,11 +34,12 @@ export class ListaPagosComponent implements OnInit {
     pagoId: 0,
     cuota: 0,
     fechaPago: '',
-    numeroSocio: 0
+    numeroSocio: 0,
   };
   socio!: Socio;
   pagoSocio!: PagoSocio;
   apellidoFiltro!: string;
+  socioFiltro!: number;
   dniFiltro!: string;
   showModalEliminarFlag: boolean = false;
   pagoId!: number;
@@ -47,7 +47,12 @@ export class ListaPagosComponent implements OnInit {
   socioIdBuscar!: number;
   ultimoPago?: Socio;
 
-  constructor(private socioService: SocioService, private pagoService: PagoService, private documentosService: DocumentosService, private dialog: MatDialog) { }
+  constructor(
+    private socioService: SocioService,
+    private pagoService: PagoService,
+    private documentosService: DocumentosService,
+    private dialog: MatDialog
+  ) {}
 
   async ngOnInit(): Promise<void> {
     await this.GetAllPagos();
@@ -55,39 +60,44 @@ export class ListaPagosComponent implements OnInit {
   }
 
   async MostrarTodosPagos(): Promise<void> {
-    return new Promise<void>(resolve => {
-      this.pagoService.mostrarTodosPagos()
-        .subscribe(async listPagos => {
-          this.listPagos = listPagos.map(pago => pago);
-          this.listPagos.sort((a: Pago, b: Pago) => b.pagoId - a.pagoId);
-          this.socioIdBuscar = this.listPagos[0].numeroSocio;
-          for (let index = 0; index < this.listSocios.length; index++) {
-            const element = this.listSocios[index];
-              if (element.numeroSocio == this.socioIdBuscar) {
-                //this.ultimoPago = this.listSocios.find(socio => socio.numeroSocio == this.socioIdBuscar);
-                this.ultimoPago = element;
-                break;
-
-              }
+    return new Promise<void>((resolve) => {
+      this.pagoService.mostrarTodosPagos().subscribe(async (listPagos) => {
+        this.listPagos = listPagos.map((pago) => pago);
+        this.listPagos.sort((a: Pago, b: Pago) => b.pagoId - a.pagoId);
+        this.socioIdBuscar = this.listPagos[0].numeroSocio;
+        for (let index = 0; index < this.listSocios.length; index++) {
+          const element = this.listSocios[index];
+          if (element.numeroSocio == this.socioIdBuscar) {
+            //this.ultimoPago = this.listSocios.find(socio => socio.numeroSocio == this.socioIdBuscar);
+            this.ultimoPago = element;
+            break;
           }
-          resolve();
-        });
+        }
+        resolve();
+      });
     });
   }
 
   async GetSocio(numeroSocio: number) {
     try {
-      const socioData = await this.socioService.getSocio(numeroSocio).toPromise();
+      const socioData = await this.socioService
+        .getSocio(numeroSocio)
+        .toPromise();
 
       // Verificar si se obtuvieron datos antes de mostrar la modal
       if (socioData) {
         this.showModalFlag = true;
-        if (socioData.direccion == "string") socioData.direccion = "";
-        if (socioData.localidad == "string") socioData.localidad = "";
-        if (socioData.fechaNacimiento == "string") socioData.fechaNacimiento = "";
-        if (socioData.fechaNacimiento == "0001-01-01") socioData.fechaNacimiento = "";
+        if (socioData.direccion == 'string') socioData.direccion = '';
+        if (socioData.localidad == 'string') socioData.localidad = '';
+        if (socioData.fechaNacimiento == 'string')
+          socioData.fechaNacimiento = '';
+        if (socioData.fechaNacimiento == '0001-01-01')
+          socioData.fechaNacimiento = '';
       } else {
-        console.error('No se encontraron datos para el número de socio:', numeroSocio);
+        console.error(
+          'No se encontraron datos para el número de socio:',
+          numeroSocio
+        );
       }
     } catch (error) {
       console.error('Error al obtener datos del socio:', error);
@@ -95,27 +105,29 @@ export class ListaPagosComponent implements OnInit {
   }
 
   async GetAllPagos(): Promise<void> {
-    return new Promise<void>(resolve => {
-      this.pagoService.getAllPagos()
-        .subscribe(listSocios => {
-          this.listSocios = listSocios.map(socio => socio);
-          this.listSocios.sort((a: Socio, b: Socio) => {
-            // Ordenar por primerApellido
-            const comparacionPrimerApellido = a.primerApellido.localeCompare(b.primerApellido);
-  
-            // Si los primeros apellidos son iguales, ordenar por segundoApellido
-            const comparacionSegundoApellido = comparacionPrimerApellido === 0
+    return new Promise<void>((resolve) => {
+      this.pagoService.getAllPagos().subscribe((listSocios) => {
+        this.listSocios = listSocios.map((socio) => socio);
+        this.listSocios.sort((a: Socio, b: Socio) => {
+          // Ordenar por primerApellido
+          const comparacionPrimerApellido = a.primerApellido.localeCompare(
+            b.primerApellido
+          );
+
+          // Si los primeros apellidos son iguales, ordenar por segundoApellido
+          const comparacionSegundoApellido =
+            comparacionPrimerApellido === 0
               ? a.segundoApellido.localeCompare(b.segundoApellido)
               : comparacionPrimerApellido;
-  
-            // Si los segundos apellidos son iguales, ordenar por nombre
-            return comparacionSegundoApellido === 0
-              ? a.nombre.localeCompare(b.nombre)
-              : comparacionSegundoApellido;
-          });
-  
-          resolve();
+
+          // Si los segundos apellidos son iguales, ordenar por nombre
+          return comparacionSegundoApellido === 0
+            ? a.nombre.localeCompare(b.nombre)
+            : comparacionSegundoApellido;
         });
+
+        resolve();
+      });
     });
   }
 
@@ -142,20 +154,27 @@ export class ListaPagosComponent implements OnInit {
 
   async ObtenerPago(numeroSocio: number) {
     try {
-      const socioData = await this.pagoService.getPagoSocio(numeroSocio).toPromise();
+      const socioData = await this.pagoService
+        .getPagoSocio(numeroSocio)
+        .toPromise();
 
       // Verificar si se obtuvieron datos antes de mostrar la modal
       if (socioData) {
         this.showModalFlag = true;
-        if (socioData.direccion == "string") socioData.direccion = "";
-        if (socioData.localidad == "string") socioData.localidad = "";
-        if (socioData.fechaNacimiento == "string") socioData.fechaNacimiento = "";
-        if (socioData.fechaNacimiento == "0001-01-01") socioData.fechaNacimiento = "";
-        if (socioData.foto == "string") socioData.foto = "";
-        socioData.fechaPago = this.formatFecha(socioData.fechaPago)
+        if (socioData.direccion == 'string') socioData.direccion = '';
+        if (socioData.localidad == 'string') socioData.localidad = '';
+        if (socioData.fechaNacimiento == 'string')
+          socioData.fechaNacimiento = '';
+        if (socioData.fechaNacimiento == '0001-01-01')
+          socioData.fechaNacimiento = '';
+        if (socioData.foto == 'string') socioData.foto = '';
+        socioData.fechaPago = this.formatFecha(socioData.fechaPago);
         this.ShowModal(socioData);
       } else {
-        console.error('No se encontraron datos para el número de socio:', numeroSocio);
+        console.error(
+          'No se encontraron datos para el número de socio:',
+          numeroSocio
+        );
       }
     } catch (error) {
       console.error('Error al obtener datos del socio:', error);
@@ -177,11 +196,10 @@ export class ListaPagosComponent implements OnInit {
   OpenPagoDialog(id: number): void {
     const dialogRef = this.dialog.open(DialogEditPagoComponent, {
       width: '400px',
-      data: { id }
+      data: { id },
     });
 
-    dialogRef.afterClosed().subscribe((result) => {
-    });
+    dialogRef.afterClosed().subscribe((result) => {});
   }
 
   EliminarPago(pagoId: number) {
@@ -192,11 +210,10 @@ export class ListaPagosComponent implements OnInit {
     try {
       await this.pagoService.deletePago(id).toPromise();
       this.HideModalEliminar();
-      alert('Pago borrado con exito.')
+      alert('Pago borrado con exito.');
       await this.GetAllPagos();
-    }
-    catch {
-      alert('No se ha podido borrar el pago.')
+    } catch {
+      alert('No se ha podido borrar el pago.');
       this.HideModalEliminar();
       await this.GetAllPagos();
     }
@@ -208,15 +225,13 @@ export class ListaPagosComponent implements OnInit {
 
   async DeletePago(id: number) {
     try {
-
       const socioData = await this.pagoService.getPagoSocio(id).toPromise();
       if (socioData != null) {
         this.showModalEliminarFlag = true;
 
         this.ShowModalEliminar(socioData);
-      }
-      else {
-        throw new Error;
+      } else {
+        throw new Error();
       }
     } catch (error) {
       console.error('Error al obtener datos del socio:', error);
--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -10,10 +10,9 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './reportes.component.html',
-  styleUrl: './reportes.component.css'
+  styleUrl: './reportes.component.css',
 })
 export class ReportesComponent implements OnInit {
-
   year!: string;
   numeroTotalSocios: number = 0;
   listSocios: Socio[] = [];
@@ -22,17 +21,7 @@ export class ReportesComponent implements OnInit {
   numeroTotalPagosAnioActual: number = 0;
   listaSociosConPagoActual!: Socio[];
   listaNumerosSocios!: number[];
-  listaYear: number[] = [
-    2024,
-    2025,
-    2026,
-    2027,
-    2028,
-    2029,
-    2030,
-    2031,
-    2032,   
-  ]
+  listaYear: number[] = [2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032];
 
   yearSeleccionado: number = 2024;
 
@@ -45,53 +34,54 @@ export class ReportesComponent implements OnInit {
     this.GetAllSocios();
     this.GetAllPagos();
   }
+
   GetAllSocios() {
     this.reportesService.ObtenerSocios().subscribe(
-      socios => {
+      (socios) => {
         this.listSocios = socios;
         this.numeroTotalSocios = this.listSocios.length;
       },
-      error => {
+      (error) => {
         console.error('Error al obtener socios:', error);
       }
     );
   }
 
   GetAllPagos() {
-    this.reportesService.ObtenerPagosAnualActual().subscribe(
-      pagos => {
-        this.listPagos = pagos;
-        //Eliminar los pagos dobles
-        // Objeto para rastrear números de socios vistos
-        const numerosSociosVistos: Record<number, boolean> = {};
+    this.reportesService.ObtenerPagosAnualActual().subscribe((pagos) => {
+      this.listPagos = pagos;
+      //Eliminar los pagos dobles
+      // Objeto para rastrear números de socios vistos
+      const numerosSociosVistos: Record<number, boolean> = {};
 
-        const arraySinDuplicados = this.listPagos.map(item => {
+      const arraySinDuplicados = this.listPagos
+        .map((item) => {
           if (!numerosSociosVistos[item.numeroSocio]) {
             numerosSociosVistos[item.numeroSocio] = true;
             return item;
           }
           return null;
-        }).filter(item => item !== null);
+        })
+        .filter((item) => item !== null);
 
-        this.numeroTotalPagosAnioActual = arraySinDuplicados.length;
+      this.numeroTotalPagosAnioActual = arraySinDuplicados.length;
 
-        for (let index = 0; index < arraySinDuplicados.length; index++) {
-          const element = arraySinDuplicados[index];
-          if (element) {
-            this.dineroTotal += element.cuota;
-          }
+      for (let index = 0; index < arraySinDuplicados.length; index++) {
+        const element = arraySinDuplicados[index];
+        if (element) {
+          this.dineroTotal += element.cuota;
         }
       }
-    )
+    });
   }
 
   async ComprobarPagosSociosAnioActual() {
     await this.reportesService.ObtenerSociosConPagoActual().subscribe(
-      socios => {
+      (socios) => {
         this.listaSociosConPagoActual = socios;
         return this.listaSociosConPagoActual;
       },
-      error => {
+      (error) => {
         console.error('Error al obtener socios:', error);
       }
     );
@@ -102,201 +92,133 @@ export class ReportesComponent implements OnInit {
     this.dineroTotal;
     for (let index = 0; index < this.listPagos.length; index++) {
       const element = this.listPagos[index];
-      this.dineroTotal = + element.cuota;
+      this.dineroTotal = +element.cuota;
     }
   }
 
-  async ImprimirListaSociosAnioActual() {
-    console.log('entra');
-    console.log(this.yearSeleccionado);
-    this.reportesService.ObtenerSociosConPagoActual().subscribe(
-      async (socios) => {
-        this.listaSociosConPagoActual = socios;
-        if (this.listSocios.length > 0) {
-
-          this.listaSociosConPagoActual.sort((a: Socio, b: Socio) => {
-            // Ordenar por primerApellido
-            const comparacionPrimerApellido = a.primerApellido.localeCompare(b.primerApellido);
-
-            // Si los primeros apellidos son iguales, ordenar por segundoApellido
-            const comparacionSegundoApellido = comparacionPrimerApellido === 0
-              ? a.segundoApellido.localeCompare(b.segundoApellido)
-              : comparacionPrimerApellido;
-
-            // Si los segundos apellidos son iguales, ordenar por nombre
-            return comparacionSegundoApellido === 0
-              ? a.nombre.localeCompare(b.nombre)
-              : comparacionSegundoApellido;
-          });
-        }
-        // Crear un nuevo documento PDF
-        const pdfDoc = await PDFDocument.create();
-        const limiteElementosPorPagina = 48;
-        let elementosEnPagina = 0;
-        let paginaActual = pdfDoc.addPage();
-
-        // Definir dimensiones de la tabla
-        const tableHeight = 14;
-
-        //Titulo del reporte
-        paginaActual.drawText("Listado de socios " + this.year + "\t\t\tTotal: " + this.listaSociosConPagoActual.length, {
-          x: 50,
-          y: 785,
-          color: rgb(0, 0.53, 0.71),
-          size: 15,
-        });
-        // Encabezado de la tabla
-        let yPosition = 750;
-        const headers = ['Primer Apellido', 'Segundo Apellido', 'Nombre', 'DNI', 'Número de Socio'];
-        for (let i = 0; i < headers.length; i++) {
-          paginaActual.drawText(headers[i], { x: 50 + i * 100, y: yPosition, color: rgb(0, 0, 0), size: 12 });
-        }
-        yPosition -= tableHeight;
-
-        // Contenido de la tabla
-        for (const socio of this.listaSociosConPagoActual) {
-          if (elementosEnPagina === limiteElementosPorPagina) {
-            elementosEnPagina = 0;
-
-            paginaActual = pdfDoc.addPage();
-
-            // Reestablecer la posición inicial para el nuevo encabezado
-            yPosition = 770;
-
-            for (let i = 0; i < headers.length; i++) {
-              paginaActual.drawText(headers[i], { x: 50 + i * 100, y: yPosition, color: rgb(0, 0, 0), size: 12 });
-            }
-            yPosition -= tableHeight;
-
-          }
-          for (let i = 0; i < headers.length; i++) {
-            paginaActual.drawText(`${socio.primerApellido}`.toUpperCase(), { x: 50 + 0 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.segundoApellido}`.toUpperCase(), { x: 50 + 1 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.nombre}`.toUpperCase(), { x: 50 + 2 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.dni}`.toUpperCase(), { x: 50 + 3 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.numeroSocio}`.toUpperCase(), { x: 50 + 4 * 100, y: yPosition - tableHeight, size: 10 });
-          }
-          yPosition -= tableHeight;
-          elementosEnPagina++;
-        }
-
-        // Guardar el documento en un Blob
-        const pdfBytes = await pdfDoc.save();
-
-        // Crear un enlace para descargar el PDF
-        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = 'ListaSociosPagoActual.pdf';
-
-        // Simular un clic en el enlace para iniciar la descarga
-        link.click();
-
-        return this.listaSociosConPagoActual;
-
-      },
-      error => {
-        console.error('Error al obtener socios:', error);
-      }
-    );
-  }
-
   async ImprimirListaSociosYearSelecionado(yearSelected: number) {
-    console.log("El año seleccionado es :" + this.yearSeleccionado);
-    this.reportesService.ObtenerSociosConPagoYearSeleccionado(yearSelected).subscribe(
-      async (socios) => {
-        this.listaSociosConPagoActual = socios;
-        if (this.listSocios.length > 0) {
+    console.log('El año seleccionado es :' + this.yearSeleccionado);
+    this.reportesService
+      .ObtenerSociosConPagoYearSeleccionado(yearSelected)
+      .subscribe(
+        async (socios) => {
+          this.listaSociosConPagoActual = socios;
+          if (this.listSocios.length > 0) {
+            this.listaSociosConPagoActual.sort((a: Socio, b: Socio) => {
+              // Ordenar por primerApellido
+              const comparacionPrimerApellido = a.primerApellido.localeCompare(
+                b.primerApellido
+              );
 
-          this.listaSociosConPagoActual.sort((a: Socio, b: Socio) => {
-            // Ordenar por primerApellido
-            const comparacionPrimerApellido = a.primerApellido.localeCompare(b.primerApellido);
+              // Si los primeros apellidos son iguales, ordenar por segundoApellido
+              const comparacionSegundoApellido =
+                comparacionPrimerApellido === 0
+                  ? a.segundoApellido.localeCompare(b.segundoApellido)
+                  : comparacionPrimerApellido;
 
-            // Si los primeros apellidos son iguales, ordenar por segundoApellido
-            const comparacionSegundoApellido = comparacionPrimerApellido === 0
-              ? a.segundoApellido.localeCompare(b.segundoApellido)
-              : comparacionPrimerApellido;
-
-            // Si los segundos apellidos son iguales, ordenar por nombre
-            return comparacionSegundoApellido === 0
-              ? a.nombre.localeCompare(b.nombre)
-              : comparacionSegundoApellido;
-          });
-        }
-        // Crear un nuevo documento PDF
-        const pdfDoc = await PDFDocument.create();
-        const limiteElementosPorPagina = 48;
-        let elementosEnPagina = 0;
-        let paginaActual = pdfDoc.addPage();
-
-        // Definir dimensiones de la tabla
-        const tableHeight = 14;
-
-        //Titulo del reporte
-        paginaActual.drawText("Listado de socios " + yearSelected + "\t\t\tTotal: " + this.listaSociosConPagoActual.length, {
-          x: 50,
-          y: 785,
-          color: rgb(0, 0.53, 0.71),
-          size: 15,
-        });
-        // Encabezado de la tabla
-        let yPosition = 750;
-        const headers = ['Primer Apellido', 'Segundo Apellido', 'Nombre', 'DNI', 'Número de Socio'];
-        for (let i = 0; i < headers.length; i++) {
-          paginaActual.drawText(headers[i], { x: 50 + i * 100, y: yPosition, color: rgb(0, 0, 0), size: 12 });
-        }
-        yPosition -= tableHeight;
-
-        // Contenido de la tabla
-        for (const socio of this.listaSociosConPagoActual) {
-          if (elementosEnPagina === limiteElementosPorPagina) {
-            elementosEnPagina = 0;
-
-            paginaActual = pdfDoc.addPage();
-
-            // Reestablecer la posición inicial para el nuevo encabezado
-            yPosition = 770;
-
-            for (let i = 0; i < headers.length; i++) {
-              paginaActual.drawText(headers[i], { x: 50 + i * 100, y: yPosition, color: rgb(0, 0, 0), size: 12 });
-            }
-            yPosition -= tableHeight;
-
+              // Si los segundos apellidos son iguales, ordenar por nombre
+              return comparacionSegundoApellido === 0
+                ? a.nombre.localeCompare(b.nombre)
+                : comparacionSegundoApellido;
+            });
           }
+          // Crear un nuevo documento PDF
+          const pdfDoc = await PDFDocument.create();
+          const limiteElementosPorPagina = 30;
+          let elementosEnPagina = 0;
+          let paginaActual = pdfDoc.addPage();
+
+          // Definir dimensiones de la tabla
+          const tableHeight = 22;
+          const columnPositions = [50, 200, 350];
+
+          //Titulo del reporte
+          paginaActual.drawText(
+            'Listado de socios ' +
+              yearSelected +
+              '\t\t\tTotal: ' +
+              this.listaSociosConPagoActual.length,
+            {
+              x: 50,
+              y: 785,
+              color: rgb(0, 0.53, 0.71),
+              size: 24,
+            }
+          );
+          // Encabezado de la tabla
+          let yPosition = 750;
+          const headers = ['1º Apellido', '2º Apellido', 'Nombre'];
           for (let i = 0; i < headers.length; i++) {
-            paginaActual.drawText(`${socio.primerApellido}`.toUpperCase(), { x: 50 + 0 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.segundoApellido}`.toUpperCase(), { x: 50 + 1 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.nombre}`.toUpperCase(), { x: 50 + 2 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.dni}`.toUpperCase(), { x: 50 + 3 * 100, y: yPosition - tableHeight, size: 10 });
-            paginaActual.drawText(`${socio.numeroSocio}`.toUpperCase(), { x: 50 + 4 * 100, y: yPosition - tableHeight, size: 10 });
+            paginaActual.drawText(headers[i], {
+              x: columnPositions[i],
+              y: yPosition,
+              color: rgb(0, 0, 0),
+              size: 18,
+            });
           }
           yPosition -= tableHeight;
-          elementosEnPagina++;
+
+          // Contenido de la tabla
+          for (const socio of this.listaSociosConPagoActual) {
+            if (elementosEnPagina === limiteElementosPorPagina) {
+              elementosEnPagina = 0;
+
+              paginaActual = pdfDoc.addPage();
+
+              // Reestablecer la posición inicial para el nuevo encabezado
+              yPosition = 770;
+              // headers
+              for (let i = 0; i < headers.length; i++) {
+                paginaActual.drawText(headers[i], {
+                  x: columnPositions[i],
+                  y: yPosition,
+                  color: rgb(0, 0, 0),
+                  size: 18,
+                });
+              }
+              yPosition -= tableHeight;
+            }
+            // Body
+            for (let i = 0; i < headers.length; i++) {
+              paginaActual.drawText(`${socio.primerApellido}`.toUpperCase(), {
+                x: columnPositions[0],
+                y: yPosition - tableHeight,
+                size: 15,
+              });
+              paginaActual.drawText(`${socio.segundoApellido}`.toUpperCase(), {
+                x: columnPositions[1],
+                y: yPosition - tableHeight,
+                size: 15,
+              });
+              paginaActual.drawText(`${socio.nombre}`.toUpperCase(), {
+                x: columnPositions[2],
+                y: yPosition - tableHeight,
+                size: 15,
+              });
+              //paginaActual.drawText(`${socio.dni}`.toUpperCase(), { x: 50 + 3 * 100, y: yPosition - tableHeight, size: 10 });
+              //paginaActual.drawText(`${socio.numeroSocio}`.toUpperCase(), { x: 50 + 4 * 100, y: yPosition - tableHeight, size: 10 });
+            }
+            yPosition -= tableHeight;
+            elementosEnPagina++;
+          }
+
+          // Guardar el documento en un Blob
+          const pdfBytes = await pdfDoc.save();
+
+          // Crear un enlace para descargar el PDF
+          const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+          const link = document.createElement('a');
+          link.href = URL.createObjectURL(blob);
+          link.download = `Listado de socios_${yearSelected}.pdf`;
+
+          // Simular un clic en el enlace para iniciar la descarga
+          link.click();
+
+          return this.listaSociosConPagoActual;
+        },
+        (error) => {
+          console.error('Error al obtener socios:', error);
         }
-
-        // Guardar el documento en un Blob
-        const pdfBytes = await pdfDoc.save();
-
-        // Crear un enlace para descargar el PDF
-        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.download = `Listado de socios_${yearSelected}.pdf`
-
-        // Simular un clic en el enlace para iniciar la descarga
-        link.click();
-
-        return this.listaSociosConPagoActual;
-
-      },
-      error => {
-        console.error('Error al obtener socios:', error);
-      }
-    );
+      );
   }
-
 }
-
-
-
-
